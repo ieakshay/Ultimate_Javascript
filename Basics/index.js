884060e78db1cf1f0813826e32ee52a46d687ee8
ieakshay <ieakshay@gmail.com>
2024-02-07 04:58:31 +0000

    notes added

--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,42 @@
-console.log("Akshay")
+// Variables types in javascript
+
+// const accountId = 46771
+// var accountPassword = "8055"
+// let accountEmail = "user@mail.com"
+// accountCity = "Jaipur"
+
+// // const accountId = 8055
+
+// console.log(typeof accountCity)
+
+// console.log(accountId)
+
+// accountPassword = "12345"
+// accountEmail = "newuser@mail.com"
+// accountCity = "Mumbai"
+
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
+// console.log(typeof accountCity)
+
+
+// Datatypes in javascript
+
+// Conversion examples and cases (Number)
+// "33" ==>> 33
+// "33abc" ==>> NaN
+// null ==>> 0
+// undefined ==>> NaN
+// true ==>> 1 : false ==>> 0
+
+// Boolean
+// 1 ==>> true : 0 ==>> false
+// "string" ==>> true
+// empty string "" ==>> false
+
+// String
+// number 11 ==>>
+
+
+
+
