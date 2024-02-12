273d3b51055ea701b9b6ce5809f5197ecdaf09fb
ieakshay <ieakshay@gmail.com>
2024-02-12 04:40:34 +0000

    array object added

--- Basics/index.js
+++ Basics/index.js
@@ -1,4 +1,4 @@
-// Variables types in javascript
+// VARIABLE types in javascript
 
 // const accountId = 46771
 // var accountPassword = "8055"
@@ -20,7 +20,7 @@
 // console.log(typeof accountCity)
 
 
-// Datatypes in javascript
+// DATATYPES in javascript
 
 // Conversion examples and cases (Number)
 // "33" ==>> 33
@@ -35,8 +35,66 @@
 // empty string "" ==>> false
 
 // String
-// number 11 ==>>
+// number 11 ==>> "11" string
+
+// let age = 24
+// let toString = String(24)
+// console.log(toString)
+// console.log(typeof toString)
+// console.log(typeof age)
 
 
+// OPERATIONS, conversions and comparisions
 
+// always  avoid below cases 
+
+// null > 0 
+// null == 0
+//numm >= 0
+
+//undefined == 0
+//undefined > 0
+//undefined < 0
+
+// string and its methods done 
+// const name = new String("neel-nitin-mukesh")
+// console.log(name)
+
+
+// Numbers and Math
+
+// Date and Time
+
+// Array and its methods
+
+// Object and its methods
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name : "User1",
+    age : 18,
+    [mySym]: "key1",     // Different syntax than other data types - steps 1)define, 2)write it as key value (key should be in square bracket), 3)then finally access it// 
+    email : "user1@mail.com",
+    isLoggedIn : true,
+    lastLoginDays : ["monday", "wedensday"]
+      
+}
+
+// console.log(JsUser.name)
+//  console.log(JsUser["email"])
+//  console.log(JsUser[mySym])
+//  console.log(typeof mySym)
+
+ JsUser.name = "User2"
+//  console.log(JsUser.name)
+
+//  Object.freeze(JsUser)  // once object is freezed its values wont change //
+
+ JsUser.greeting = function(){
+    console.log("Hello")
+ }
+
+ console.log(JsUser.greeting)
+ console.log(JsUser.greeting())
 
