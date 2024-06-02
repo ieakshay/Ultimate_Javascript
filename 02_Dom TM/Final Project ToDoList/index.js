fd4e07b459be56d6590d36910907eb19689017a1
ieakshay <ieakshay@gmail.com>
2024-06-02 11:40:53 +0000

    Delete and search functions addded

--- 02_Dom TM/Final Project ToDoList/index.js
+++ 02_Dom TM/Final Project ToDoList/index.js
@@ -1,8 +1,16 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // form submit event
 form.addEventListener('submit', addItem);
+
+// Delete event
+itemList.addEventListener('click', removeItem);
+
+// filter item
+filter.addEventListener('keyup', filterItems)
+
 
 //add item
 function addItem(e){
@@ -18,5 +26,46 @@ li.className = "list-group-item"
 //add textNode with input value
 li.appendChild(document.createTextNode(newItem))
 
-itemList.appendChild(li)
+// create delete button
+var deleteBtn = document.createElement('button');
+
+// add class to delete button
+deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+//Append text node
+deleteBtn.appendChild(document.createTextNode('X'));
+
+// Append button to li
+li.appendChild(deleteBtn);
+
+// Append li to list
+itemList.appendChild(li);
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+       if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+       }
+    }
+}
+
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+
+    //get lis
+    var items = itemList.getElementsByTagName('li')
+
+    // Connvert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else{
+            item.style.display = 'none';
+        }
+
+    })
 }
